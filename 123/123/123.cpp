c642484eac4fda15ec1ea58be873af8b049c34bc
limlet <leminako2009@gmail.com>
2025-12-06 21:35:32 +0300

    add solve

---
+++ 123/123/123.cpp
@@ -0,0 +1,33 @@
+﻿//#define _USE_MATH_DEFINES
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <math.h>
+#include <set>
+#include <map>
+#include <iomanip>
+#include <fstream>
+#include <queue>
+#include <string>
+#include <deque>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+using ll = long long;
+
+vector<int> v;
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n; cin >> n;
+    for (int& i : v) {
+        cin >> i;
+    }
+
+}
